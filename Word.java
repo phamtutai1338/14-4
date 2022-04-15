f5f2068428490cf0cbe141023c8949208b9f980d
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2022-04-15 15:46:56 +0700

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Word {
+    private String value;
+    private int position;
+    public Word(String v,int p){
+        value=v;
+        position=p;
+    }
+    public String getValue() {
+        return value;
+    }
+    public int getPosition() {
+        return position;
+    }
+
+    }
